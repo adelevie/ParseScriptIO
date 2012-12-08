3d711fdef3d141613aeb25804c6b35550da32a5e
Alan deLevie <adelevie@gmail.com>
2012-12-08 17:30:15 -0500

    getting a feel for lua--adding base uri constructing method

--- parse.lua
+++ parse.lua
@@ -1,16 +1,31 @@
--- mymodule.lua
-local M = {} -- public interface
+parse = {}
 
--- private
-local x = 1
-local function baz() print 'test' end
+parse.client = {}
+function parse.client.new(keys)
+  local self = {
+    application_id = keys.application_id,
+    rest_api_key = keys.rest_api_key,
+    version = '1',
+    host = 'https://api.parse.com'
+  }
 
-function M.foo() print("foo", x) end
-
-function M.bar()
-  M.foo()
-  baz()
-  print "bar"
+  function self:base_uri()
+    self.host + "/" + self.version
+  end
 end
 
-return M
+
+
+
+return parse
+
+--[[
+
+client = parse.client.new({
+  'application_id' = '123456',
+  'rest_api_key' = 'abcdefgh'
+})
+
+client.get('/classes/Post/12345')
+
+]]--
