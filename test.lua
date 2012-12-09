21d2f52bbdee3a058994c82c8cd25ef21c4c9f22
Alan deLevie <adelevie@gmail.com>
2012-12-09 16:17:11 -0500

    even dumber test 'framework'

--- test.lua
+++ test.lua
@@ -1,28 +1,33 @@
-local parse = require("parse")
+local parse = require("parse") -- requring the thing to test
 
+-- stupid test "framework"
 local assert_equal = function(good_value, mystery_value)
-	if good_value == mystery_value then
-		return "Passed"
-	else
-		return mystery_value .. " does not equal " .. good_value .. " (expected)."
-	end
+  if good_value == mystery_value then
+    return "Passed"
+  else
+    error(mystery_value .. " does not equal " .. good_value .. " (expected).")
+  end
 end
-
-local test_functions = {
-	test_base_uri = function()
-		result = parse.protocol.base_uri()
-		return assert_equal("https://api.parse.com/1/", result)
-	end,
-
-	test_something_else = function()
-		return assert_equal(1, 4)
-	end
-}
 
 local function run_tests(tests)
-	for k,v in pairs(tests) do
-		print(k .. ": " .. v())
-	end
+  for k,v in pairs(tests) do
+    print(k .. ": " .. v())
+  end
 end
+-- end stupid test framework
 
-run_tests(test_functions)
+-- functions to test
+local test_functions = {
+  test_base_uri = function()
+    result = parse.protocol.base_uri()
+    return assert_equal("https://api.parse.com/1/", result)
+  end,
+
+  test_something_else = function()
+    return assert_equal(4, 4)
+  end
+}
+-- end functions to test
+
+
+run_tests(test_functions) -- run the tests!
