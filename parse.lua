72e44dca4b0ab4d36d0e3bf979bf17e9ce8a1416
Alan deLevie <adelevie@gmail.com>
2012-12-09 11:34:30 -0500

    ugh

--- parse.lua
+++ parse.lua
@@ -1,13 +1,16 @@
-local Parse = {}
-local Parse["client"] = {}
+local Parse = {
+  client = {
+    application_id = nil
+  }
+}
 
 function Parse.init()
-  Parse.client["application_id"] = "1"
+  Parse.client.application_id = "1"
   print "init!"
 end
 
 function Parse.client:get()
-  if not self.client.application_id then
+  if not Parse.client.application_id then
     error("Client not initialized.")
   else then
     print "get!"
