2580d07c262803d5f122e60c0d86c4001234d4fe
Alan deLevie <adelevie@gmail.com>
2012-12-09 11:09:35 -0500

    added missing comma

--- parse.lua
+++ parse.lua
@@ -2,7 +2,7 @@ parse = {}
 
 parse.protocol = {
   version  = '1',
-  host     = 'https://api.parse.com'
+  host     = 'https://api.parse.com',
   base_url = function() return parse.protocol.host .. "/" .. parse.protocol.version .. "/" end
 }
 
